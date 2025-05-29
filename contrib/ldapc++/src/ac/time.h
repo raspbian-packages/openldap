d74396435ab8673be57bfd8363651475c42dfcd4
Ryan Tandy <ryan@nardis.ca>
2025-05-29 16:41:48 -0700

    Import openldap_2.6.10+dfsg.orig.tar.xz
    [dgit import orig openldap_2.6.10+dfsg.orig.tar.xz]

---
+++ contrib/ldapc++/src/ac/time.h
@@ -0,0 +1,24 @@
+/* Generic time.h */
+/* $OpenLDAP$ */
+/*
+ * Copyright 1998-2024 The OpenLDAP Foundation, Redwood City, California, USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted only
+ * as authorized by the OpenLDAP Public License.  A copy of this
+ * license is available at http://www.OpenLDAP.org/license.html or
+ * in file LICENSE in the top-level directory of the distribution.
+ */
+
+#ifndef _AC_TIME_H
+#define _AC_TIME_H
+
+#if defined HAVE_SYS_TIME_H
+# include <sys/time.h>
+# ifdef HAVE_SYS_TIMEB_H
+#  include <sys/timeb.h>
+# endif
+#endif
+# include <time.h>
+
+#endif /* _AC_TIME_H */
